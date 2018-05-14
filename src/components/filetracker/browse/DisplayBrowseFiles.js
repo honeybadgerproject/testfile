84660477c202f3f94f5d3cda3f4b19995e88e075
Yabin Monroy <yabinmonroy@Yabins-MacBook-Air.local>
2018-05-14 07:42:50 -0700

    new commit

--- src/components/filetracker/browse/DisplayBrowseFiles.js
+++ src/components/filetracker/browse/DisplayBrowseFiles.js
@@ -6,18 +6,60 @@ import StickerWidget from "../../../containers/Widgets/sticker/sticker-widget";
 import { StickerWidgetWrapper } from '../../../containers/Widgets/sticker/style';
 import folderList from './folderList';
 import Scrollbars from '../../utility/customScrollBar.js';
+import MailBox from './mailBox.style';
+import singleMail from '../../mail/singleMail';
 
 import {
   printBrowseFiles
 } from '../../../redux/filetracker/actions';
 
+
 class DisplayBrowseFiles extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: this.props.searchString,
+    };
+  }
   componentDidMount() {
 
   }
   render() {
+    const {
+      allMails,
+      filterAttr,
+      filterMails,
+      selectedMail,
+      composeMail,
+      replyMail,
+      selectMail,
+      filterAction,
+      changeComposeMail,
+      changeReplyMail,
+      changeSearchString,
+    } = this.props;
+    const { search } = this.state;
+    let singleMailComponent = (
+      <p className="isoNoMailMsg">
+
+      </p>
+    );
+    const index = allMails.findIndex(mail => mail.id === selectedMail);
+    if (index !== -1) {
+      singleMailComponent = singleMail(
+        allMails,
+        filterMails,
+        index,
+        replyMail,
+        changeReplyMail,
+        selectMail
+      );
+    }
     return (
-      <div>
+      <MailBox className="isomorphicMailBox">
+        <div className="isoLeftWrapper">
+
+        </div>
         <div className="isoMiddleWrapper">
           <div className="isoBucketLabel">
             <h3>bucket 1</h3>
@@ -31,7 +73,12 @@ class DisplayBrowseFiles extends Component {
             {folderList()}
           </Scrollbars>
         </div>
-      </div>
+        <div className="isoSingleMailWrapper">
+          <Scrollbars style={{ height: this.props.height - 70 }}>
+
+          </Scrollbars>
+        </div>
+      </MailBox>
     );
   }
 }
--- src/components/filetracker/browse/folderList.js
+++ src/components/filetracker/browse/folderList.js
@@ -66,7 +66,7 @@ export default function mailList(
   };
   return (
     <FolderListWrapper className="isoMailListWrapper">
-       {mails.map((mail, index) => renderSingleMail(mail, index))}
+       
     </FolderListWrapper>
   );
 }
